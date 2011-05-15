46a1a6787f66fd5a5de6d244febaabfdedec3393
britg <brit@britg.com>
2011-05-14 21:07:31 -0500

    research docs

---
+++ WTMGlyph/WTMGlyphStroke.h
@@ -0,0 +1,16 @@
+//
+//  WTMGlyphStroke.h
+//  WTMGlyph
+//
+//  Created by Brit Gardner on 5/14/11.
+//  Copyright 2011 Warrior Thief Mage Studios. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface WTMGlyphStroke : NSObject {
+    
+}
+
+@end
---
+++ WTMGlyph/WTMGlyphStroke.m
@@ -0,0 +1,14 @@
+//
+//  WTMGlyphStroke.m
+//  WTMGlyph
+//
+//  Created by Brit Gardner on 5/14/11.
+//  Copyright 2011 Warrior Thief Mage Studios. All rights reserved.
+//
+
+#import "WTMGlyphStroke.h"
+
+
+@implementation WTMGlyphStroke
+
+@end
